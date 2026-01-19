e2d078d096fcb09d51984435530d58a774b791c3
ashisentry <ashisentry@gmail.com>
2026-01-19 09:09:07 +0000

    Initial Maven app

---
+++ devsecops-demo/src/main/java/com/xfusioncorp/demo/App.java
@@ -0,0 +1,13 @@
+package com.xfusioncorp.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
